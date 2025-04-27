5546caccc432365a979e60b59401f8855f5c22ff
Kyra4code <lucarioecharmito@gmail.com>
2025-04-27 13:42:10 -0300

    Feito uma lista dos 151 pokemons na tela de time

--- src/components/obj/PokeCards.jsx
+++ src/components/obj/PokeCards.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
 import style from "./PokeCards.module.css"
 
-const PokeCards = ({PokeName}) => {
+const PokeCards = ({PokeName, PokeImage}) => {
     return(
         <div className={style.container}>
+            <img src={PokeImage} alt={PokeName} className={style.img}/>
             <h5>{PokeName}</h5>
         </div>
     );
--- src/components/pages/Teams.jsx
+++ src/components/pages/Teams.jsx
@@ -5,16 +5,29 @@ import axios from "axios";
 
 export default function Teams(){
     
-    let pokeDados;
+    const [pokeDados, setPokeDados] = useState([]);
 
-    useEffect(async()=>{
-        pokeDados = await axios.get("https://pokeapi.co/api/v2/pokemon?offset=0&limit=151")
-        console.log(pokeDados.data)
+
+    useEffect(()=>{
+        const fetchData = async () => {
+            let pokeList = [];
+            for(let i = 1; i <= 151; i++){
+                pokeList.push(`https://pokeapi.co/api/v2/pokemon/${i}/`);
+            }
+            await axios.all(pokeList.map(poke => axios.get(poke))).then((res) => setPokeDados(res));
+        }
+
+        fetchData();
     },[])
 
+    {if(pokeDados.length === 0){
+        return <div className={styles.loading}>Carregando...</div>;
+    }}
     return(
         <div className={styles.container}>
-            <PokeCards PokeName="Pikachu"/>
+            {pokeDados.map((pokemon)=>(
+                <PokeCards PokeName={pokemon.data.name} PokeImage={pokemon.data.sprites.front_default}/>
+            ))}
         </div>
     );
 }
